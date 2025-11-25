86faddcedff228933e71766180bb2b05791ac9f0
harisislam10 <islamharis87@gmail.com>
2025-11-25 17:42:48 +0800

    Adding Downlaod script to download kernel binaries

---
+++ downloads.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+echo "=== Removing old release files ==="
+
+# Remove old single file
+rm -f hailo_volumes.tar.gz
+
+# Ensure tar/ exists
+mkdir -p tar
+
+# Remove old files inside tar/
+rm -f tar/kernel-source.tar.gz
+rm -f tar/kernel-build.tar.gz
+
+echo "=== Downloading latest GitHub releases ==="
+
+# URLs
+URL_HAILO="https://github.com/harisislam10/edgemind-software/releases/download/release-2025-11-26/hailo_volumes.tar.gz"
+URL_KERNEL_BUILD="https://github.com/harisislam10/edgemind-software/releases/download/release-2025-11-26/kernel-build.tar.gz"
+URL_KERNEL_SOURCE="https://github.com/harisislam10/edgemind-software/releases/download/release-2025-11-26/kernel-source.tar.gz"
+
+# Download files
+wget -O hailo_volumes.tar.gz "$URL_HAILO"
+wget -O tar/kernel-build.tar.gz "$URL_KERNEL_BUILD"
+wget -O tar/kernel-source.tar.gz "$URL_KERNEL_SOURCE"
+
+echo "=== Downloads completed successfully ==="
